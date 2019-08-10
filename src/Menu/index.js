37814f878c6401f7dca1fdb6e78f2de30e3d23d4
Gustaw <tanski.gustaw@gmail.com>
2019-08-10 17:10:04 +0200

    Essential methods added

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,10 +1,18 @@
 // Odpowiedzialność: Kontener łączący interfejs i logikę menu gry
+// ! UWAGA ta klasa jest atrapą, trzeba będzie ją jeszcze zaimplementować
 
 class Menu {
-
-    start() {
-
-    }
+	constructor() {}
+	// ustawia callback _onStart i przekazuje go w dół
+	setOnStart(onStart) {}
+	// przyłącza menu do DOMu w wyznaczonym węźle
+	mount(node) {}
+	// odłącza od DOMu
+	unmount() {}
+	// przywraca do stanu początkowego
+	hardReset() {}
+	// czyści wybrane przez użytkownika własciwości, ale nie te ustawione przez programiste
+	softReset() {}
 }
 
 export default Menu;
