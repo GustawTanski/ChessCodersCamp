e63e5bfcaa885683a8e42b0e5faffbca406406c9
Izabela Popiel <izabelapopiel1@gmail.com>
2019-08-11 21:14:18 +0200

    knight branch added

---
+++ src/Game/gameLogic/pieces/Knight.js
@@ -0,0 +1,10 @@
+//klasa reprezentaująca figurę skoczka
+//skoczek porusza się po literze 'L' - krok o jedno pole w pionie lub poziomie, a następnie drugi krok na ukos
+//skoczek ignoruje figury stojące na jego drodze, przeskakuje przez nie
+
+import Piece from "./Piece";
+class Knight extends Piece {
+
+}
+
+export default Knight;
