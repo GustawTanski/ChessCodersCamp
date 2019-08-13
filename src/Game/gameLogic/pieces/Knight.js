216a4ee88477959a4b6865fed47d8d9dc9f0e557
Izabela Popiel <izabelapopiel1@gmail.com>
2019-08-13 20:06:31 +0200

    legalMoves() method added

--- src/Game/gameLogic/pieces/Knight.js
+++ src/Game/gameLogic/pieces/Knight.js
@@ -5,6 +5,56 @@
 import Piece from "./Piece";
 class Knight extends Piece {
 
+    constructor(position, color, movementPoints, moveDirection) {
+        super(position, color, movementPoints, moveDirection);
+    }
+
+    //metoda zwracająca tablicę z dostępnymi opcjami ruchu
+    //skoczek może ruszyć się maksymalnie na 8 dostepnych pozycji
+    //należy wyeliminować pozycje poza szachownicą
+    //oraz pozycje na których stoją figury tego samego koloru co dany skoczek
+    legalMoves(boardState) {
+
+        const possiblePositions = this._allPossiblePositions;
+
+        //eliminacja pozycji poza szachownicą
+        const onBoardPositions = possiblePositions.filter(pos => {
+            return !Piece._isOutOfTheBoard(pos);
+        });
+
+        //eliminacja pozycji na których już stoi figura tego samego koloru co skoczek - nie zbijamy samych siebie
+        const boardState2D = boardState.toTwoDimensionArray;
+        const legalPositions = onBoardPositions.filter(pos => {
+            //pieceOnBoard figura na szachownicy na pozycji którą może zająć skoczek
+            const pieceOnBoard = boardState2D[pos.x][pos.y];
+            return pieceOnBoard.color != this._color;
+        });
+
+        //zwraca legalne pozycje - tablicę elementów Coords
+        return legalPositions;
+    }
+
+
+    //zwraca wszystkie 8 możliwych pozycji po ruchu skoczka
+    _allPossiblePositions() {
+        const possiblePositions = [];
+        for (let i = -2; i <= 2; i += 4) {
+            for (let j = -1; j <= 1; j += 2) {
+
+                possiblePositions.push({
+                    x: this._position.x + i,
+                    y: this._position.y + j
+                });
+
+                possiblePositions.push({
+                    x: this._position.x + j,
+                    y: this._position.y + i
+                });
+            }
+        }
+        return possiblePositions;
+    }
+
 }
 
 export default Knight;
