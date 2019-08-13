03a4c8664e8164b67f563ea1efcb1d348c91b643
Irina <rishabubela@gmail.com>
2019-08-13 22:31:37 +0200

    Added a class Bishop

---
+++ src/Game/gameLogic/pieces/Bishop.js
@@ -0,0 +1,13 @@
+// Zawiera klasę figury gońca
+// Zasady:
+/////1. poruszanie się po przekątnych.
+/////2. nie może przeskakiwać przez inne figury
+/////3. może zbijać figury przeciwnika.
+
+import Piece from './Piece'
+class Bishop extends Piece {
+
+}
+
+
+export default Bishop;
