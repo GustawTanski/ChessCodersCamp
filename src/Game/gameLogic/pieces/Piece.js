30ab2301e41f8027c5386872674e722b2b6c6dca
Bartosz <bartosz.burdalski@gmail.com>
2019-08-13 19:56:27 +0200

    small changes

--- src/Game/gameLogic/pieces/Piece.js
+++ src/Game/gameLogic/pieces/Piece.js
@@ -3,55 +3,47 @@
 // Wszystkie klasy z folderu Pieces to klasy logiczne - służą do obliczeń i wewnętrzenej mechaniki gry, nie do
 // wyświetlania, ani tworzenia interfejsu
 class Piece {
-	constructor(position, color, movementPoints, moveDirection) {
-		//zamiast podawanej właściwości name, bedzie ona pobierana z właściwości konstruktora .name
-		// np. const pionek = new Piece(x,y,z...) pionek.constructor.name
+    constructor(position, color) {
+        if (this.constructor === Piece) {
+            throw new Error("Nie możesz tworzyć obiektów z klasy abstrakcyjnej!");
+        }
+        //np. {x:3,y:1}...
+        this._position = position;
+        // np. black or white
+        this._color = color;
+        // możliwe promocje
+        // zmieniłem na wielkie litery - bo nazwy klas GUSTAW
+        this.possiblePromotion = ["Queen", "Rook", "Bishop", "Knight"];
 
-		//np. [2,5],[x=1,y=3],[x:3,y:1]...
-		this._position = position;
-		// np. black or white
-		this._color = color;
-		// kierunki w których może poruszać się pionek np. forwardOnly,diagonally,leftAndRight
-		this._moveDirection = moveDirection;
-		//o ile pól pionek może się poruszyć np. pawn ma 2, knight ma 1, rook ma 7
-		this._movementPoints = movementPoints;
-		// możliwe promocje
-		// zmieniłem na wielkie litery - bo nazwy klas GUSTAW
-		this.possiblePromotion = ["Queen", "Rook", "Bishop", "Knight"];
+        this.promoted = false;
 
-		// dodałem, bo w sumie to może gdzieś się przyda GUSTAW
-		this._name = this.constructor.name;
+        // dodałem, bo w sumie to może gdzieś się przyda GUSTAW
+        this._name = this.constructor.name;
 
-		this.promoted = false;
-	}
-	get position() {
-		return this._position;
-	}
-	get color() {
-		return this._color;
-	}
-	get moveDirection() {
-		return this._moveDirection;
-	}
-	get movementPoints() {
-		return this._movementPoints;
-	}
+        this._movementPoints = null;
+    }
+    get position() {
+        return this._position;
+    }
+    get color() {
+        return this._color;
+    }
+    get name() {
+        return this._name;
+    }
 
-	get name() {
-		return this._name;
-	}
+    move(toCoords) {
+        //metoda odpowiedzialna za poruszanie się pionka
+    }
 
-	moveChess() {
-		//metoda odpowiedzialna za poruszanie się pionka
-	}
+    legalMoves(boardState) {
+        // metoda sprawdzająca możliwe ruchy
+    }
 
-	checkMoveChess() {
-		// metoda sprawdzająca czy dany ruch jest możliwy
-	}
-
-	pieceLoss() {
-		// metoda wywoływana w momencie gdy nasz pionek został zbity
-	}
+    pieceLoss() {
+        // metoda wywoływana w momencie gdy nasz pionek został zbity
+        // this._position={x:null,y:null}
+    }
 }
 
 export default Piece;
