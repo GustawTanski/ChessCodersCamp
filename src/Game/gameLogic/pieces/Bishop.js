ac47dcc43a185c9ecd8165322369963c0ec04bc7
Irina <rishabubela@gmail.com>
2019-08-15 14:32:49 +0200

    Added an algorithm (diagonal movement) and some classes

--- src/Game/gameLogic/pieces/Bishop.js
+++ src/Game/gameLogic/pieces/Bishop.js
@@ -7,7 +7,67 @@
 import Piece from './Piece'
 class Bishop extends Piece {
 
-}
+    constructor(position, color) {
+        super(position, color);
+    }
 
+    legalMoves(boardState) {
+        // metoda sprawdzająca możliwe ruchy
+    }
+
+    pieceLoss() {
+        // metoda wywoływana w momencie gdy nasz pionek został zbity
+        // this._position={x:null,y:null}
+    }
+
+
+    _allPossiblePositions() {
+        const possiblePositions = [];
+
+
+        possiblePositions.push({
+            //dodanie możliwych pozycji do listy
+
+        })
+        // Up right
+        // if (this._position.y > this._position.x) {
+        //     y = 7;
+        //     x = 7 - (y - x)
+        // } else {
+        //     y = 7 - (x - y);
+        //     x = 7;
+        // }
+
+        // // Down left
+        // if (x > y) {
+        //     y = 0;
+        //     x = 0 + (x - y)
+        // } else {
+        //     y = 0 + (y - x);
+        //     x = 0;
+        // }
+
+        // // Up left
+        // if (8 - x < y) {
+        //     y = 7;
+        //     x = (x + y) - 7;
+        // } else {
+        //     y = (x + y) - 0;
+        //     x = 0;
+        // }
+
+        // // Down right
+        // if (8 - x > y) {
+        //     y = 0;
+        //     x = (x + y) - 0;
+        // } else {
+        //     y = (x + y) - 7;
+        //     x = 7;
+        // }
+
+    }
+
+
+}
 
 export default Bishop;
