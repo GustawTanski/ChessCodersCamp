7e59443634a0724d02c97a64d5f23a340c43e704
GustawTanski <47717934+GustawTanski@users.noreply.github.com>
2019-08-19 16:58:23 +0200

    Revert "Merge pull request #5 from GustawTanski/Piece"
    This reverts commit 646a5090baea8d33536f69ef6b46a6671f3c2e65.

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,18 +1,10 @@
 // Odpowiedzialność: Kontener łączący interfejs i logikę menu gry
-// ! UWAGA ta klasa jest atrapą, trzeba będzie ją jeszcze zaimplementować
 
 class Menu {
-	constructor() {}
-	// ustawia callback _onStart i przekazuje go w dół
-	setOnStart(onStart) {}
-	// przyłącza menu do DOMu w wyznaczonym węźle
-	mount(node) {}
-	// odłącza od DOMu
-	unmount() {}
-	// przywraca do stanu początkowego
-	hardReset() {}
-	// czyści wybrane przez użytkownika własciwości, ale nie te ustawione przez programiste
-	softReset() {}
+
+    start() {
+
+    }
 }
 
 export default Menu;
