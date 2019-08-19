b69bd5bd06007648598d78ba28863f285405a6de
Krzysztof <dydakkrzysztof@gmail.com>
2019-08-19 22:26:34 +0200

    Working Rook class

--- src/Game/gameLogic/pieces/Rook.js
+++ src/Game/gameLogic/pieces/Rook.js
@@ -1,7 +1,9 @@
 //klasa reprezentująca 'Wieżę' 
 //Wieża porusza się po liniach pionowych i poziomych, w dowolnym kierunku, o dowolną liczbę niezajętych pól. 
 import Piece from "./Piece";
+import { isDeepStrictEqual } from "util";
 
+// Obiekt służacy do określenia wymiarów szachownicy
 const chessBoard = {
     length: 8,
     height: 8
@@ -13,56 +15,57 @@ class Rook extends Piece{
         super(position,color)
     }
 
+    //metoda zwracające tablice współrzędnych, z dostępnymi ruchami pionka
     legalMoves(boardState){
+
         const boardState2D = boardState.toTwoDimensionArray();
         var possibleMoves = []
-
-        // Tworzy możliwe ruchy bez uwzględnienia pozostalych pionków
-        for (let possibleX = this.position.x - 1; possibleX >= 0; possibleX--) {
-            if(!boardState2D[possibleX][this.position.y]){
-                possibleMoves.push({
-                    x: possibleX,
-                    y: this.position.y
-                })
-            } else {
-                break;
-            }
-        }   
-
-        for (let possibleX = this.position.x + 1; possibleX < chessBoard.length; possibleX++) {
-            if(!boardState2D[possibleX][this.position.y]){
-                possibleMoves.push({
-                    x: possibleX,
-                    y: this.position.y
-                })
-            } else {
-                break;
-            }
-        }   
-
-        for (let possibleY = this.position.y -1; possibleY >= 0; possibleY--) {
-            if(!boardState2D[this.position.x][possibleY]){
-                possibleMoves.push({
-                    x: this.position.x,
-                    y: possibleY
-                })
-            } else {
-                break;
-            }
-        }
-
-        for (let possibleY = this.position.y + 1; possibleY < chessBoard.height; possibleY++) {
-            if(!boardState2D[this.position.x][possibleY]){
-                possibleMoves.push({
-                    x: this.position.x,
-                    y: possibleY
-                })
-            } else {
-                break;
-            }
-        }
+ 
+        this._checkLegalPositionsInDirection(boardState2D, possibleMoves,this.position.x - 1,-1,-1,'x')
+        this._checkLegalPositionsInDirection(boardState2D, possibleMoves,this.position.x + 1,chessBoard.length,1,'x')
+        this._checkLegalPositionsInDirection(boardState2D, possibleMoves,this.position.y - 1,-1,-1,'y')
+        this._checkLegalPositionsInDirection(boardState2D, possibleMoves,this.position.y + 1,chessBoard.height,1,'y')
 
         return possibleMoves;
+    }
+
+    // metoda iterująca po polach szachownicy w jednych z określonych kierunków (N,E,S,W)
+    // sprawdza czy dany pionek jest innego koloru niż wybrany pionek i wówczas gdy tak jest
+    // to go dodaje do tablicy `possibleMoves`
+    _checkLegalPositionsInDirection(boardState, possibleMoves, startingCoordinate, endingCoordinate, incrementValue, xOrY){
+        var element;
+
+        for (let coordinate = startingCoordinate; coordinate !== endingCoordinate; coordinate += incrementValue) {
+            xOrY.toLowerCase() === 'x' ? element = boardState[coordinate][this.position.y]
+            :   element = boardState[this.position.x][coordinate]
+
+            if(!element){
+                this._pushElement(possibleMoves,xOrY,this.position.x,this.position.y,coordinate)                
+                
+            } else if(element.color !== this.color){
+                this._pushElement(possibleMoves,xOrY,this.position.x,this.position.y,coordinate)
+                break;
+
+            } else{
+                break;
+            }
+        }   
+    }
+
+    // metoda dodaje do tablicy `array` jedną współrzędną stałą oraz jedną iterowaną.
+    // Iterowaną współrzędną jest ta ktora została określona w parametrze `iteratingOverXorY`
+    _pushElement(array, iteratingOverXorY, baseX, baseY, iteratingCoordinate){
+        if(iteratingOverXorY.toLowerCase() === 'x'){
+            array.push({
+                x: iteratingCoordinate,
+                y: baseY
+            })
+        } else {
+            array.push({
+                x: baseX,
+                y: iteratingCoordinate
+            })
+        }
     }
 }
 
