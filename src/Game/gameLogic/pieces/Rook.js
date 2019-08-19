1ef7031109acbd6db274f00131a1f419a5bbfd6a
Krzysztof <dydakkrzysztof@gmail.com>
2019-08-19 14:43:24 +0200

    Building legalMoves

--- src/Game/gameLogic/pieces/Rook.js
+++ src/Game/gameLogic/pieces/Rook.js
@@ -2,8 +2,31 @@
 //Wieża porusza się po liniach pionowych i poziomych, w dowolnym kierunku, o dowolną liczbę niezajętych pól. 
 import Piece from "./Piece";
 
+const chessBoard = {
+    length: 8,
+    height: 8
+}
+
 class Rook extends Piece{
 
+    constructor(position, color){
+        super(position,color)
+    }
+
+    legalMoves(boardState){
+        console.log('legalMoves')
+        const boardState2D = boardState.toTwoDimensionArray();
+
+        for (let possibleX = 0; possibleX < chessBoard.length; possibleX++) {
+            boardState2D[possibleX,this.position.y] = 'X'
+        }   
+
+        for (let possibleY = 0; possibleY < chessBoard.length; possibleY++) {
+            boardState2D[this.position.x,possibleY] = 'Y'
+        }   
+
+        console.log(boardState);
+    }
 }
 
 export default Rook;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import App from "./App";
+import Rook from "./Game/gameLogic/pieces/Rook";
 
 //w tym pliku inicjalizowana będzie aplikacja i prawdopobodnie nic więcej
 
 const app = new App();
+const rook = new Rook({x:0,y:0},'black');
+
+rook.legalMoves();
 
